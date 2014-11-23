a0aaa776fa466116b69abc0ad0b166c7d50232cf
Kristel Teng <kristelteng@gmail.com>
2014-11-23 13:37:11 -0500

    Finish Jquery

--- jquery_fundamentals/jquery_exercises.js
+++ jquery_fundamentals/jquery_exercises.js
@@ -1,83 +1,171 @@
 // SOLUTIONS GO BELOW EACH EXERCISE
 
-// EXERCISE: Change Background
 
+// EXERCISE: Change Background
 // Change the background color of '#target' by script. 
 // Hint: you can use .css
 
-// EXERCISE: Change Parent
+// $(document).ready(function() {
+//   $("#target").css("background-color", "yellow");
+// });
+             
 
+
+// EXERCISE: Change Parent
 // Change the text in the span, a child of "#target"
 // Hint: you can use .text
 
-// EXERCISE: Create Clone
+// $(document).ready(function() {
+//   $("span").text("I was changed!");
+// });
 
+
+
+// EXERCISE: Create Clone
 // Create a clone of the span in "#target" and position it under the origin.
 // Hint: you can use .clone and .insertAfter
 
-// EXERCISE: Use Filter
+// $(document).ready(function() {
+//   $('#target').clone().insertAfter('#target');
+// });
 
+
+
+// EXERCISE: Use Filter
 // Change background color of the second ".target"
 // Hint: you can use .eq and .css
 
-// EXERCISE: Disable Buttons
+// $(document).ready(function() {
+//   $('.target').eq(1).css("background-color", "yellow");
+// });
 
+
+
+// EXERCISE: Disable Buttons
 // Disable the button
 // Hint: you will have to select the button and can use .attr
 
-// EXERCISE: Uncheck CheckBoxes
+// $(document).ready(function() {
+//   $(":button").attr("disabled", "disabled");
+// });
 
+
+
+// EXERCISE: Uncheck CheckBoxes
 // Uncheck all checkboxes using jQuery
 // Hint: you will have to select the input and can use .removeAttr
 
-// EXERCISE: Change Parent
+// $(document).ready(function () {
+//   $(":input").removeAttr("onclick", null);
+// });
 
+
+
+// EXERCISE: Change Parent
 // Move "#child" from "#parent1" to "#parent2"
 // Hint: you can use .appendTo
 
-// EXERCISE: Select A Option in A Select Box
+// $(document).ready(function () {
+//   $("#child").appendTo("#parent2");
+// });
 
+
+
+// EXERCISE: Select A Option in A Select Box
 // Select the second option in the selectbox by script
 // Hint: you will have to select the option and can use .eq and .attr
 
-// EXERCISE: Change Size
+// $(document).ready(function () {
+//   $("select option").eq(1).attr("selected", true);
+// });
 
+
+
+// EXERCISE: Change Size
 // Make "#target" double size
 // Hint: you can use .css, .width and .height
+
+// $(document).ready(function () {
+//   $("#target").css ({"width": "300px", "height" : "200px"}); 
+// });
+
+
 
 // EXERCISE: Empty Elements
 // Hint: you can use .empty
 
 // Remove all children and text of "#target"
+// $(document).ready(function () {
+//   $("#target").empty();
+// });
+
+
 
 // EXERCISE: Delay
-
 // Show Alert with 1 second delay (Use "setTimeout")
 // Hint: you can use .setTimeout and alert
 
-// EXERCISE: Count
+// $(document).ready(function () {
+//   setTimeout(function(){
+//     alert("hello")}, 1000);
+// });
 
+
+
+
+// EXERCISE: Count
 // Show the number of children in an alert
 // Hint: you can use .children, .length and alert or you could use .find, .size and alert. the alert should show 5
 
-// EXERCISE: Animate
+// $(document).ready(function () {
+//   var n = $("#target").find(".child").size();
+//   alert("Children: " + n);
 
+// });
+
+
+
+// EXERCISE: Animate
 // Make "#target" double size with animation
 // Hint: you can use .animate, .width, and .height
 
-// EXERCISE: Alternate Color
+// $(document).ready(function () {
+//   $("#target").animate({
+//     "width": "300px",
+//     "height": "200px"
+//   }, 2000);
+// });
 
+
+       
+// EXERCISE: Alternate Color
 // Make the list-box alternating. (Make odd options a different color)
 // Hint: you can select the option and use .filter and .css
 
-// EXERCISE: All But One
+// $(document).ready(function () {
+//  $('select option').filter(":odd").css("background-color", "yellow");
+// });
 
+
+
+// EXERCISE: All But One
 // Remove all children of "#target" but h2
 // Hint: you can use .children, .not and .remove
+
+// $(document).ready(function () {
+//  var c = $("#target").children("");
+//  c.not("h2").remove();
+// });
+
+
 
 // EXERCISE: Without Children
 
 // Remove all 'div' whitch has no child elements.
 // Hint: you can use .not and .remove. Look closely at what options you can pass .not
 
+// $(document).ready(function () {
+//  $("div:empty").remove();
+
+// });
 
